60b7041bd4753af4f35b92bce0f49db5898851ae
duygueroglu <duygu-1999-@hotmail.com>
2022-09-07 14:46:45 +0300

    app.js updated

--- src/App.js
+++ src/App.js
@@ -7,40 +7,52 @@ import {Footer, Navbar, Sidebar, ThemeSettings} from './components'
 import {Area, Bar, Calendar, ColorMapping, ColorPicker, Customers, Ecommerce, Editor, Employees, Financial, Kanban, Orders, Pie, Pyramid, Stacked} from './pages'
 import './App.css'
 
+import {useStateContext} from './contexts/ContextProvider'
 
 const App = () => {
-    const activeMenu = false
+    const {activeMenu, currentColor, currentMode, setCurrentColor, setCurrentMode, setThemeSettings, themeSettings}= useStateContext();
+    
+    useEffect(()=>{
+        const currentThemeColor=localStorage.getItem('colorMode');
+        const currentThemeMode=localStorage.getItem('themeMode');
+        if(currentThemeColor&&currentThemeMode){
+            setCurrentColor(currentThemeColor)
+            setCurrentMode(currentThemeMode)
+        }
+    },[])
     return(
-        <div>
+        <div className={currentMode=='Dark'?'dark':''}>
             <BrowserRouter>
                 <div className="flex relative dark:bg-main-dark-bg">
                     <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
                         <TooltipComponent content="Settings" position="Top">
-                            <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background: 'blue', borderRadius: '50%'}}>
+                            <button type="button" onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: '50%' }} className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white">
                                 <FiSettings/>
                             </button>
                         </TooltipComponent>
                     </div>
                     {activeMenu ? (
                         <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-                            Sidebar
+                            <Sidebar/>
                         </div>
                     ) : (
                         <div className="w-0 dark:bg-secondary-dark-bg">
-                            Sidebar w-0
+                            <Sidebar/>
                         </div>
                     )
                 }
                     <div className={
-                        `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72'
-                        :
-                        'flex-2'}`
-                    }>
+              activeMenu
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+            }>
                         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
                             Navbar
                         </div>
                     </div>
                     <div>
+                    {themeSettings && (<ThemeSettings />)}
                         <Routes>
                             {/* Dashboard */}
                             <Route path="/" element="ECommerce "/>
