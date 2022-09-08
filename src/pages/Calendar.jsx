fb181018b7b73f1befde2e774ded9ddffc369ecc
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-09-08 23:59:37 +0300

    Update Calendar.jsx

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
+import { Agenda, Day, DragAndDrop, Inject, ScheduleComponent, ViewsDirective, ViewDirective, Week, WorkWeek, Month, Resize } from '@syncfusion/ej2-react-schedule';
 
 const Calendar = () => {
+  const [scheduleObj, setScheduleObj] = useState();
+
+  const change = (args) => {
+    scheduleObj.selectedDate = args.value;
+    scheduleObj.dataBind();
+  };
+
+  const onDragStart = (arg) => {
+    arg.navigation.enable = true;
+  };
   return (
     <div>Calendar</div>
   )
