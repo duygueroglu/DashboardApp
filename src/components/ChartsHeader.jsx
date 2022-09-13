136eec06d55828c2acf9d114a5f2f362c8371a55
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-09-13 23:08:17 +0300

    chartsheader updated

--- src/components/ChartsHeader.jsx
+++ src/components/ChartsHeader.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const ChartsHeader = () => {
+const ChartsHeader = ({category, title}) => {
   return (
-    <div>ChartsHeader</div>
+    <div className=" mb-10">
+    <div>
+      <p className="text-lg text-gray-400">Chart</p>
+      <p className="text-3xl font-extrabold tracking-tight dark:text-gray-200 text-slate-900">{category}</p>
+    </div>
+    <p className="text-center dark:text-gray-200 text-xl mb-2 mt-3">{title}</p>
+  </div>
   )
 }
 
