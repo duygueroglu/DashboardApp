0384f73782c42f9511575bd5e00be42bf60d8d6f
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-09-18 01:51:40 +0300

    footer.jsx updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">© 2022 Duygu Eroğlu</p>
+  </div>
   )
 }
 
