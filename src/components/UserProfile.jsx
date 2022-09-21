34b5717bc5a1a99b8ca254d61ba565f353d3c97a
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-09-21 15:40:43 +0300

    userprofile.jsx updated

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {MdOutlineCancel} from 'react-icons/md'
+import {Button} from '.'
+import {userProfileData} from '../data/dummy'
+import {useStateContext} from '../contexts/ContextProvider'
+import avatar from '../data/avatar.jpg'
 
 const UserProfile = () => {
+  const {currentColor} = useStateContext();
   return (
     <div>UserProfile</div>
   )
