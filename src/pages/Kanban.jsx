14678d9abc98f86fe32c1bf14cb7a770c226eec0
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-09-21 15:48:24 +0300

    kanban.jsx updated

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {ColumnDirective, ColumnsDirective, KanbanComponent} from '@syncfusion/ej2-react-kanban'
+import {Header} from '../components';
+import {kanbanData, kanbanGrid} from '../data/dummy';
 
-const Kanban = () => {
-  return (
-    <div>Kanban</div>
-  )
-}
+const Kanban = () => (
+  
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+         <Header category="App" title="Kanban" />
+         <KanbanComponent dataSource={kanbanData} id="kanban" keyField="Status" cardSettings={{contentField:'Summary', headerField:'Id'}}>
+         <ColumnsDirective>
+             {
+                 kanbanGrid.map((item, index) => 
+                     <ColumnDirective key={index} {...item}/>
+                 )
+             }
+         </ColumnsDirective>
+         </KanbanComponent>
+    </div>
+  
+)
 
 export default Kanban
