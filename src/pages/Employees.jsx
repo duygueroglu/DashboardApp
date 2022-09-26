9d2a127910fa18e8482abb2fc14c32c1bb232886
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-09-26 23:31:48 +0300

    employees.jsx updated

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {ColumnDirective, ColumnsDirective, GridComponent, Inject, Page, Search } from '@syncfusion/ej2-react-grids';
+import {employeesData, employeesGrid} from '../data/dummy';
+import {Header} from '../components';
 
 const Employees = () => {
+  const toolbarOptions = ['Search'];
+  const editing = {allowDeleting: true, allowEditing: true};
   return (
-    <div>Employees</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <Header category="Page" title="Employees" />
+        <GridComponent dataSource={employeesData} width="auto" allowPaging allowSorting pageSettings={{pageCount: 5}}
+        editSettings={editing}
+        toolbar={toolbarOptions}>
+            <ColumnsDirective>
+            {employeesGrid.map((item, index) => 
+                <ColumnDirective key={index} {...item}/>)}
+            </ColumnsDirective>
+        <Inject services={[Search, Page]}/>
+      </GridComponent>
+    </div>
   )
 }
 
