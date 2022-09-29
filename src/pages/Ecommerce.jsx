246b557637f4d6deae13b47d0991fd63dd0a0845
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-09-29 19:57:30 +0300

    ecommerce.jsx updated

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import {BsCurrencyDollar} from 'react-icons/bs';
+import {GoPrimitiveDot} from 'react-icons/go';
+import {IoIosMore} from 'react-icons/io';
+import {DropDownListComponent} from '@syncfusion/ej2-react-dropdowns';
+
+import {Stacked, Pie, Button, LineChart, SparkLine} from '../components';
+import {earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData} from '../data/dummy';
+import {useStateContext} from '../contexts/ContextProvider';
+import product9 from '../data/product9.jpg';
 
 const Ecommerce = () => {
+  const {currentColor,currentMode } = useStateContext();
   return (
     <div>Ecommerce</div>
   )
